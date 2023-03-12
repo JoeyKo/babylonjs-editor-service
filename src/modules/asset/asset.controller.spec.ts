4c4c8e9acbe38df336400360da196306c58a09b9
joey <joeyko100gmail.com>
2023-03-12 15:44:45 +0800

    fix: asset controller e2e test

--- src/modules/asset/asset.controller.spec.ts
+++ src/modules/asset/asset.controller.spec.ts
@@ -1,20 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { AssetController } from './asset.controller';
-import { AssetService } from './asset.service';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from '../../app.module';
 
-describe('AssetController', () => {
-  let controller: AssetController;
+describe('AssetController (e2e) Query', () => {
+  let app: INestApplication;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [AssetController],
-      providers: [AssetService],
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
     }).compile();
 
-    controller = module.get<AssetController>(AssetController);
+    app = moduleFixture.createNestApplication();
+    await app.init();
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('/v1/asset (GET)', async () => {
+    const result = await request(app.getHttpServer()).get('/v1/asset');
+    expect(result.body).toBeTruthy();
   });
 });
